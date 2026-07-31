3f8417189bd4b36d46a40db4c64df0ccf097283f
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-31 23:18:55 +0600

    added projects section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero/Hero";
+import Projects from "@/components/Projects/Projects";
 import Skills from "@/components/Skills/Skills";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="px-60">
       <Hero />
       <Skills />
+      <Projects />
     </div>
   );
 }
