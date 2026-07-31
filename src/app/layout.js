91ce30f6d8727ca8278f9621ee1ad41d01a64130
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-31 17:53:13 +0600

    added header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, Geist, Geist_Mono, Rufina } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const dmSans = DM_Sans({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin']
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const rufina = Rufina({
+  weight: ['400', '700'],
+  subsets: ['latin']
 });
 
 export const metadata = {
@@ -20,9 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${dmSans.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
+const navItems = [
+  { id: 1, text: "Home", link: "/" },
+  { id: 2, text: "About", link: "/about" },
+  { id: 3, text: "Contact", link: "/contact" },
+  { id: 4, text: "Projects", link: "/projects" },
+];
+
 const Header = () => {
+    const pathname = usePathname()
     return (
-        <div>
-            
+        <div className='flex items-center justify-between px-28 py-5'>
+            <ul className='flex items-center gap-7'>
+                {
+                    navItems?.map(item => (
+                        <li key={item?.id}>
+                            <Link href={item.link} className={`${item?.link === pathname ? 'font-semibold text-[#4E433D]' : 'font-medium text-[#4E433D]'}`}>{item.text}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+            <Link href={'/'} className='capitalize font-medium border border-[#D2D2D2] bg-[#F9F8F6] rounded-full px-3 py-1.5 flex gap-2 text-[#251D18] items-center shadow-sm cursor-pointer'>
+                <span>Hire me</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" color="#000000" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M18.5 12L4.99997 12"></path>
+                    <path d="M13 18C13 18 19 13.5811 19 12C19 10.4188 13 6 13 6"></path>
+                </svg>
+            </Link>
         </div>
     );
 };
