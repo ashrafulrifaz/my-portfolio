5f071e8d45de3103ba50a9cb93183db20457377e
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-08-02 02:07:54 +0600

    fixed skills section responsiveness

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -29,14 +29,14 @@ const hoverEffect = {
 
 const Skills = () => {
     return (
-        <div className='h-[70vh] flex flex-col justify-center items-center relative'>
+        <div className='h-[90vh] lg:h-[70vh] flex flex-col justify-center items-center relative'>
             <h6 className='text-[#251D18] text-lg capitalize font-medium'>Always Building <br /> always growing</h6>
 
             <div>
                 <motion.div
                     {...floatAnimation(4, 0)}
                     whileHover={hoverEffect}
-                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black absolute top-1/2 right-[10%] lg:right-[20%] cursor-pointer'
+                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black absolute top-[60%] lg:top-1/2 right-[30%] lg:right-[20%] cursor-pointer'
                 >
                     <Image src={'/assets/skills/better-auth.png'} alt='better-auth logo' width={50} height={50} className='w-14 h-14' />
                 </motion.div>
@@ -44,7 +44,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(3.5, 0.3)}
                     whileHover={hoverEffect}
-                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black absolute bottom-4 lg:bottom-20 left-[7%] lg:left-1/5 flex items-center justify-center cursor-pointer'
+                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black absolute bottom-10 lg:bottom-20 left-1/4 lg:left-1/5 flex items-center justify-center cursor-pointer'
                 >
                     <Image src={'/assets/skills/figma.png'} alt='figma logo' width={50} height={50} className='w-14 h-20' />
                 </motion.div>
@@ -52,7 +52,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(4.2, 0.6)}
                     whileHover={hoverEffect}
-                    className='rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] absolute top-10 lg:top-20 right-[5%] lg:right-[30%] border border-[#d2d2d2] flex items-center justify-center cursor-pointer'
+                    className='rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] absolute top-10 right-[30%] border border-[#d2d2d2] flex items-center justify-center cursor-pointer'
                 >
                     <Image src={'/assets/skills/firebase.png'} alt='firebase logo' width={50} height={50} className='w-10 h-10 rounded-lg' />
                 </motion.div>
@@ -60,7 +60,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(3.8, 0.9)}
                     whileHover={hoverEffect}
-                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black flex items-center justify-center absolute top-5 lg:top-10 left-2 lg:left-5 cursor-pointer'
+                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black flex items-center justify-center absolute top-5 lg:top-10 left-5 cursor-pointer'
                 >
                     <Image src={'/assets/skills/framer.jpg'} alt='framer logo' width={50} height={50} className='w-14 h-14' />
                 </motion.div>
@@ -68,7 +68,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(4.5, 0.2)}
                     whileHover={hoverEffect}
-                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black flex items-center justify-center absolute bottom-3 lg:bottom-10 right-7 lg:right-10 cursor-pointer'
+                    className='rounded-2xl overflow-hidden w-14 h-14 bg-black flex items-center justify-center absolute bottom-5 lg:bottom-10 right-7 lg:right-10 cursor-pointer'
                 >
                     <Image src={'/assets/skills/react.png'} alt='react logo' width={50} height={50} className='w-10 h-10' />
                 </motion.div>
@@ -76,7 +76,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(3.6, 0.5)}
                     whileHover={hoverEffect}
-                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute bottom-1/5 lg:top-1/4 left-2/5 lg:right-15 cursor-pointer"
+                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute top-1/4 right-5 lg:right-15 cursor-pointer"
                 >
                     <Image src={'/assets/skills/js.png'} alt='js logo' width={50} height={50} className='w-10 h-10 rounded-lg' />
                 </motion.div>
@@ -84,7 +84,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(4.1, 0.8)}
                     whileHover={hoverEffect}
-                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute top-1/4 left-2/5 lg:left-1/4 cursor-pointer"
+                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute top-1/4 left-1/4 cursor-pointer"
                 >
                     <Image src={'/assets/skills/github-logo.png'} alt='github logo' width={50} height={50} className='w-10 h-10 rounded-lg' />
                 </motion.div>
@@ -92,7 +92,7 @@ const Skills = () => {
                 <motion.div
                     {...floatAnimation(3.9, 0.4)}
                     whileHover={hoverEffect}
-                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute top-1/2 left-10 lg:left-16 cursor-pointer"
+                    className="rounded-2xl overflow-hidden w-14 h-14 bg-[#F9F8F6] flex items-center justify-center border border-[#d2d2d2] absolute top-[59%] lg:top-1/2 left-8 lg:left-16 cursor-pointer"
                 >
                     <Image src={'/assets/skills/Next.js.png'} alt='nextjs logo' width={50} height={50} className='w-10 h-10 rounded-lg' />
                 </motion.div>
